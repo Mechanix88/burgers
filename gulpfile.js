c2b2509e3228ee4ca5b1f5d5404d3658412ae083
mehcnaix96 <Mechanix96@mail.ru>
2019-06-04 14:27:40 +0300

    Форма,карта

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp        = require('gulp'), 
-    sass        = require('gulp-sass'); 
+    sass        = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    browserSync = require('browser-sync');
 
 gulp.task('sass', function(){ 
     return gulp.src('sass/**/*.{sass,scss}') 
@@ -11,4 +14,34 @@ gulp.task('watch', function() {
     gulp.watch('sass/**/*.{sass,scss}' , gulp.parallel('sass'));
 });
 
-gulp.task('default', gulp.parallel('watch'))﻿
+gulp.task('default', gulp.parallel('watch'))
+
+gulp.task('default', function () {
+return gulp.src('css/*')
+
+.pipe( autoprefixer({
+overrideBrowserslist: ['last 2 versions'],
+cascade: false
+}))
+.pipe( gulp.dest('css') );
+
+});
+
+
+
+gulp.task('default', () =>
+    gulp.src('img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img'))
+);
+
+
+gulp.task('browser_sync', function () {
+    browserSync({
+    port: 9000,
+    open: true,
+    notify: false,
+    server: 'burger'
+    
+    });
+    });
